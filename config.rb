d7b72715807fec128b59d2288041653757a44e2e
Cecile Veneziani <contact@cecilitse.org>
2015-07-14 01:07:57 +0200

    Add homepage

--- config.rb
+++ config.rb
@@ -1,7 +1,12 @@
 activate :bower
 
+set :css_dir,     'assets/stylesheets'
+set :images_dir,  'assets/images'
+set :js_dir,      'assets/javascripts'
 
 configure :build do
+  activate :minify_css
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
