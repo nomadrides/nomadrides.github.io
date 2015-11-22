1c2bcdf6cb515501b97eafbdb3ab63c12200a7fa
Cecile Veneziani <contact@cecilitse.org>
2015-11-22 23:38:46 +0100

    Switch from Foundation 5 to Foundation Sites 6

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,4 +1,10 @@
 //= require "jquery/dist/jquery"
-//= require "foundation/js/foundation/foundation"
-//= require "foundation/js/foundation/foundation.topbar"
+
+//= require "foundation-sites/js/foundation.core"
+//= require "foundation-sites/js/foundation.util.keyboard"
+//= require "foundation-sites/js/foundation.util.mediaQuery"
+//= require "foundation-sites/js/foundation.util.nest"
+//= require "foundation-sites/js/foundation.dropdownMenu"
+//= require "foundation-sites/js/foundation.responsiveToggle"
+
 //= require_tree .
