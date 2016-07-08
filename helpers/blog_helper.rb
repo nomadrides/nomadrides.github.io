5ba06369d9ba1c7fe4f47fbf093c4ea6d2092d45
Cecile Veneziani <contact@cecilitse.org>
2016-07-08 19:16:09 +0200

    Refactor frontend architecture

--- helpers/blog_helper.rb
+++ helpers/blog_helper.rb
@@ -2,4 +2,8 @@ module BlogHelper
   def blog_article_page?
     !current_page.directory_index?
   end
+
+  def blog_page_css_class_name
+    blog_article_page? ? 'p-blog-article' : 'p-blog-index'
+  end
 end
